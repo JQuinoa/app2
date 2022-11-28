6c0dcaf2bd0f5eb535235431b02db51c8e364cda
JQuinoa <jacobo.quinoa@gmail.com>
2022-11-28 21:57:03 +0100

    Creación de la class Bill

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 class Bill:
-
-    def __init__(self, amount):
+    """
+    Object that contains data about a bill, such as total amount and period of the bill
+    """
+    def __init__(self, amount, period):
         self.amount = amount
+        self.period = period
+        
+
+
+class flatm
 
 bill = Bill(120)
 print(bill.amount)
