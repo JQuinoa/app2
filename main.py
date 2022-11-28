95fd1b47c0c008f3d475566e18b3ce25174ac1b5
JQuinoa <jacobo.quinoa@gmail.com>
2022-11-28 22:55:06 +0100

    Se introduce el cálculo de la cuantía

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ class Flatmate:
         self.days_in_house = days_in_house
         self.name = name
 
-    def pays(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
@@ -35,4 +37,5 @@ the_bill = Bill(amount=120, period = "March 2021")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
-print(john.pays(bill=the_bill))
+print("John pays:", john.pays(bill=the_bill, flatmate2=marry))
+print("Marry pays:", marry.pays(bill=the_bill, flatmate2=john))
