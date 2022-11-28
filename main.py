3d4dc702e157c5817b423e52ad4005cc7854ec7a
JQuinoa <jacobo.quinoa@gmail.com>
2022-11-28 22:20:53 +0100

    Creación de la class PdfReport, y con ello la columna vertebral de la aplicación

--- main.py
+++ main.py
@@ -21,5 +21,13 @@ class Flatmate:
 
 class PdfReport:
     """
-    Creates a Pdf file that contains data
+    Creates a Pdf file that contains data about the flatmates such as their names,
+    their due amounts and the period of the bill.
     """
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+
